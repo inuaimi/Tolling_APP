e46f2e9445685fe6c097851dc5197ea68e95c752
Alexander Linné <21100826+lajne@users.noreply.github.com>
2019-04-12 18:00:14 +0200

    added pod 2

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -12,7 +12,20 @@ export default class SecondScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>MapScreen</Text>
+        <View style={styles.map}>
+          <Text style={styles.text}>MapScreen</Text>
+        </View>
+        <View style={styles.bottomDetailsContainer}>
+          <View style={styles.bottomDetailsKeys}>
+            <Text style={styles.bottomDetailsKeyText}>Name of gantry</Text>
+            <Text style={styles.bottomDetailsKeyText}>Distance to gantry</Text>
+          </View>
+          <View style={styles.bottomDetailsValues}>
+          <Text style={styles.bottomDetailsText}>Öresund</Text>
+          <Text style={styles.bottomDetailsText}>10km</Text>
+          
+          </View>
+        </View>
       </View>
     );
   }
--- Styles/styles.js
+++ Styles/styles.js
@@ -13,4 +13,36 @@ export default StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  map: {
+    flex: 5,
+    justifyContent: 'center',
+    backgroundColor: '#2ed573',
+  },
+
+  // #bottomDetails
+  bottomDetailsContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    backgroundColor: '#FFF'
+  },
+  bottomDetailsKeys: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    // backgroundColor: '#ff7f50'
+  },
+  bottomDetailsValues: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    // backgroundColor: '#eccc68'
+  },
+  bottomDetailsKeyText: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  bottomDetailsValueText: {
+    fontSize: 14,
+  }
 });
