8dfb1562cf1bcf424e73be6c09c1c3980e25f65d
Alexander Linné <21100826+lajne@users.noreply.github.com>
2019-05-09 11:47:05 +0200

    fetch transactions and render

--- Screens/TransactionsScreen.js
+++ Screens/TransactionsScreen.js
@@ -4,6 +4,7 @@ import { Text, View, StyleSheet, ScrollView } from "react-native";
 import styles from "../Styles/styles";
 import { Header, Card, ListItem, Divider } from "react-native-elements";
 import DeviceInfo from "react-native-device-info";
+import { db } from '../Database/Database';
 
 export default class SecondScreen extends React.Component {
   static navigationOptions = {
@@ -11,13 +12,67 @@ export default class SecondScreen extends React.Component {
   };
   constructor(props) {
     super(props);
+    this.ref = db.collection('Users').doc('XO5lwKAyI3PaEpGQ2bZ4');
+    this.unsubscribe = null;
+
     this.state = {
       gantrys: [],
-      balance: Number
+      balance: Number,
+      ready: false
     };
   }
 
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+    // DeviceInfo.getMACAddress().then(val => {
+    //   console.log("MacAdress: " + val);
+    // });
+    // let gantry1 = { name: "Gantry1", date: "2019-02-11", cost: 20, id: "0" };
+    // let gantry2 = { name: "Gantry2", date: "2019-02-11", cost: 38, id: "1" };
+    // let gantry3 = { name: "Gantry3", date: "2019-02-11", cost: 38, id: "2" };
+    // let gantry4 = { name: "Gantry4", date: "2019-02-11", cost: 38, id: "3" };
+    // let gantry5 = { name: "Gantry5", date: "2019-02-11", cost: 38, id: "4" };
+    // let gantry6 = { name: "Gantry6", date: "2019-02-11", cost: 38, id: "5" };
+    // let gantry7 = { name: "Gantry7", date: "2019-02-11", cost: 38, id: "6" };
+    // let gantry8 = { name: "Gantry8", date: "2019-02-11", cost: 38, id: "7" };
+    // let gantry9 = { name: "Gantry9", date: "2019-02-11", cost: 38, id: "8" };
+    // let gantry10 = { name: "Gantry10", date: "2019-02-17", cost: 38, id: "9" };
+    // let gantry11 = { name: "Gantry11", date: "2019-02-17", cost: 40, id: "10" };
+    // this.setState({
+    //   gantrys: [
+    //     gantry1,
+    //     gantry2,
+    //     gantry3,
+    //     gantry4,
+    //     gantry5,
+    //     gantry6,
+    //     gantry7,
+    //     gantry8,
+    //     gantry9,
+    //     gantry10,
+    //     gantry11
+    //   ]
+    // });
+    this.setState({ balance: 319 });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (doc) => {
+    const user = doc.data();
+
+    this.setState({
+      transactions: user.transactions,
+      ready: true
+    });
+  }
+
   render() {
+    if(!this.state.ready) {
+      return null;
+    }
     return (
       <View style={localStyles.mainContainer}>
         <ScrollView>
@@ -33,39 +88,6 @@ export default class SecondScreen extends React.Component {
     );
   }
 
-  componentDidMount() {
-    DeviceInfo.getMACAddress().then(val => {
-      console.log("MacAdress: " + val);
-    });
-    let gantry1 = { name: "Gantry1", date: "2019-02-11", cost: 20, id: "0" };
-    let gantry2 = { name: "Gantry2", date: "2019-02-11", cost: 38, id: "1" };
-    let gantry3 = { name: "Gantry3", date: "2019-02-11", cost: 38, id: "2" };
-    let gantry4 = { name: "Gantry4", date: "2019-02-11", cost: 38, id: "3" };
-    let gantry5 = { name: "Gantry5", date: "2019-02-11", cost: 38, id: "4" };
-    let gantry6 = { name: "Gantry6", date: "2019-02-11", cost: 38, id: "5" };
-    let gantry7 = { name: "Gantry7", date: "2019-02-11", cost: 38, id: "6" };
-    let gantry8 = { name: "Gantry8", date: "2019-02-11", cost: 38, id: "7" };
-    let gantry9 = { name: "Gantry9", date: "2019-02-11", cost: 38, id: "8" };
-    let gantry10 = { name: "Gantry10", date: "2019-02-17", cost: 38, id: "9" };
-    let gantry11 = { name: "Gantry11", date: "2019-02-17", cost: 40, id: "10" };
-    this.setState({
-      gantrys: [
-        gantry1,
-        gantry2,
-        gantry3,
-        gantry4,
-        gantry5,
-        gantry6,
-        gantry7,
-        gantry8,
-        gantry9,
-        gantry10,
-        gantry11
-      ]
-    });
-    this.setState({ balance: 319 });
-  }
-
   renderMoneyBalance() {
     return (
       <Card title="Balance">
@@ -79,16 +101,16 @@ export default class SecondScreen extends React.Component {
   }
 
   renderPassedGantrys() {
-    console.log("gantrys:", this.state.gantrys);
+    const { transactions } = this.state;
     return (
       <Card title="Passed gantrys">
-        {this.state.gantrys.map(x => {
+        {this.state.transactions.map((transaction, key) => {
           return (
-            <View key={x.id}>
+            <View key={key}>
               <ListItem
-                title={x.name}
-                subtitle={x.date}
-                rightSubtitle={"-" + x.cost + "kr"}
+                title={transaction.gantry}
+                subtitle={transaction.date}
+                rightSubtitle={"-" + transaction.cost + "kr"}
                 subtitleStyle={{ color: "#707070" }}
               />
               <Divider />
@@ -102,7 +124,8 @@ export default class SecondScreen extends React.Component {
   //Not used atm
   calculateMoneySpent() {
     let totalMoneySpent = 0;
-    this.state.gantrys.forEach(x => {
+    const { transactions } = this.state;
+    transactions.forEach(x => {
       totalMoneySpent += x.cost;
     });
 
