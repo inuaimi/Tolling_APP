bc961fedd082b51e04ff7b4864e09cce1331bf27
laban211 <lalaban211@gmail.com>
2019-05-16 17:27:35 +0200

    Some more minor fixes

--- App.js
+++ App.js
@@ -8,9 +8,7 @@
 
 import React from "react";
 //Main navigation system
-// import MainNavigator from "./Navigation/MainNavigator";
 import { createMainNavigator } from "./Navigation/MainNavigator";
-// import AsyncStorage from "@react-native-community/async-storage";
 import firebase from "react-native-firebase";
 
 export default class App extends React.Component {
@@ -27,10 +25,7 @@ export default class App extends React.Component {
   }
 
   async componentDidMount() {
-    // let val = await this.checkIfLoggedIn();
     this.checkIfLoggedIn();
-    console.log("_application isloggedIn: ", val);
-    // this.setState({ isLoggedIn: val });
   }
 
   checkIfLoggedIn = () => {
@@ -42,16 +37,4 @@ export default class App extends React.Component {
       }
     });
   };
-
-  // checkIfLoggedIn = async () => {
-  //   try {
-  //     const val = await AsyncStorage.getItem("isLoggedIn");
-  //     if (val === "true") {
-  //       return true;
-  //     }
-  //     return false;
-  //   } catch {
-  //     //Handle error
-  //   }
-  // };
 }
